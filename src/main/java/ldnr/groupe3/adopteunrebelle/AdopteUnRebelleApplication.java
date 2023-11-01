b03bc5c06f96d2f64cd0e303d1d30b15afd1b54d
David J <david.a.jonquieres@sogeti.com>
2023-11-01 12:26:37 +0100

    Création du projet + gitIngnore

---
+++ src/main/java/ldnr/groupe3/adopteunrebelle/AdopteUnRebelleApplication.java
@@ -0,0 +1,13 @@
+package ldnr.groupe3.adopteunrebelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopteUnRebelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopteUnRebelleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ldnr/groupe3/adopteunrebelle/AdopteUnRebelleApplicationTests.java
@@ -0,0 +1,13 @@
+package ldnr.groupe3.adopteunrebelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopteUnRebelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
