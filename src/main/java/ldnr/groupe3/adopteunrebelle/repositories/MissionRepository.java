c3cd0a72002db44130416815f1eacfaa14a87bec
chloe <chguilet199@gmail.com>
2023-11-02 09:46:58 +0100

    #6 Creation MissionRepository

---
+++ src/main/java/ldnr/groupe3/adopteunrebelle/repositories/MissionRepository.java
@@ -0,0 +1,7 @@
+package ldnr.groupe3.adopteunrebelle.repositories;
+
+import ldnr.groupe3.adopteunrebelle.models.Mission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<Mission, Integer> {
+}
